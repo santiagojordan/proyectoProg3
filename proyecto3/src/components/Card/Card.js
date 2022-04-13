3c04d6b7b81109a684af3d9a14f8893c85fbf79f
santiagojordan <sjordan@udesa.edu.ar>
2022-04-13 12:15:49 -0300

    components

---
+++ proyecto3/src/components/Card/Card.js
@@ -0,0 +1,63 @@
+import React, {Component} from "react";
+import "./Card.css"
+
+class Card extends Component{
+constructor(props){
+    super(props)
+    this.state={
+        link: " ",
+        text: "Ver más",
+        viewMore: false,
+    };
+
+}
+
+    viewMore() {
+        if (this.state.viewMore) {
+            this.setState({
+                viewMore: false,
+                text: "Ver más",
+            });
+        } else {
+            this.setState({
+                viewMore: true,
+                text: "Ver menos",
+            });
+        }
+    }
+
+render(){
+    return(
+        <section className="todo">
+        <div className="container">
+            
+            
+                <h3>{this.props.dataPelis.title}</h3>
+                <img src={this.state.link + this.props.dataPelis.poster_path} alt={this.props.dataPelis.title} />
+                <p>Descripcion: {this.props.dataPelis.overview}</p>
+          
+               
+                <section
+                    className={`${this.state.viewMore ? "aditional-info-show" : "aditional-info"
+                        }`}
+                >
+                    
+                    <p>Rating: {this.props.dataPelis.vote_average}</p>
+                    <p>Estreno: {this.props.dataPelis.release_date}</p>
+                    <p>Popularidad: {this.props.dataPelis.popularity}</p>
+                
+                </section>
+                <button className="more" onClick={() => this.viewMore()}>
+                    {this.state.text}
+                </button>
+                <button onClick={() => this.props.borrarPeliculas(this.props.dataPelis.id)}>Borrar</button>
+               
+                
+        </div>
+        </section>
+    )
+}
+
+}
+
+export default Card;
