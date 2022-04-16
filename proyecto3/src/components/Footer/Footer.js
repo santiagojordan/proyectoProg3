bac25af7d0316691012d0af47ea6835274211b9e
Tomas Jarsun <toto@Totos-Air.localdomain>
2022-04-16 02:22:27 -0300

    footer done

---
+++ proyecto3/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Footer/footer.css"
+
+function Footer() {
+    return (
+        <footer>
+            <ul className="team">
+                <li>Tomás Jarsun</li>
+                <li>Santiago Jordan</li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
