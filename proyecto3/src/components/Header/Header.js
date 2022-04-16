47cd5386c32256ee3a4492bb4a2ae02bb5c97420
Tomas Jarsun <toto@Totos-Air.localdomain>
2022-04-16 16:27:20 -0300

    header inc

---
+++ proyecto3/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css";
+
+function Header (){
+    return (
+
+        <div className="title">
+            <img src=""></img>
+     </div>
+
+     );
+}
+
+export default Header;
